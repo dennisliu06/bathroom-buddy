92a456f571685605d9ab2a8466e33b7563e10f73
dennis <dennisliu010906@gmail.com>
2024-03-03 16:19:12 -0500

    Added map in explorer

---
+++ client/src/components/modules/Map.js
@@ -0,0 +1,35 @@
+import React from "react";
+import GoogleMapReact from 'google-map-react';
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+export default function SimpleMap(){
+  const defaultProps = {
+    center: {
+      lat: 42.36042881656016,
+      lng: -71.0873451394108
+    },
+    zoom: 17
+  };
+
+  return (
+    // Important! Always set the container height explicitly
+    <>
+        <div className="Map-container">
+            <div style={{height: '95vh', width: '50%' }}>
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: "AIzaSyCJPdR_z8r1nFaV9BgQX18PV8BZDqib0ho" }}
+                defaultCenter={defaultProps.center}
+                defaultZoom={defaultProps.zoom}
+            >
+                <AnyReactComponent
+                lat={59.955413}
+                lng={30.337844}
+                text="My Marker"
+                />
+            </GoogleMapReact>
+            </div>
+        </div>
+    </>
+  );
+}
--- client/src/components/pages/Explore.js
+++ client/src/components/pages/Explore.js
@@ -1,16 +1,73 @@
 import React from "react";
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
 
 import "../../utilities.css";
 import "./Explore.css"
 import NavBar from "../modules/NavBar.js";
+import Map from "../modules/Map.js";
 
-const Explore = (props) => {
-    return (
+const containerStyle = {
+  width: '400px',
+  height: '400px'
+};
+
+const center = {
+  lat: -3.745,
+  lng: -38.523
+};
+
+/*const Explore = (props) => {
+    const { isLoaded } = useJsApiLoader({
+      id: 'Maps-API-Key',
+      googleMapsApiKey: "AIzaSyCJPdR_z8r1nFaV9BgQX18PV8BZDqib0ho"
+    })
+    
+  
+    const [map, setMap] = React.useState(null)
+  
+    const onLoad = React.useCallback(function callback(map) {
+      // This is just an example of getting and using the map instance!!! don't just blindly copy!
+      const bounds = new window.google.maps.LatLngBounds(center);
+      map.fitBounds(bounds);
+        
+      setMap(map)
+    }, [])
+
+    useEffect(() => {
+      
+    }, [])
+  
+    const onUnmount = React.useCallback(function callback(map) {
+      setMap(null)
+    }, [])
+  
+    return isLoaded ? (
       <>
         <NavBar />
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={10}
+          onLoad={onLoad}
+          onUnmount={onUnmount}
+        >
+          {}
+          <></>
+        </GoogleMap>
+      </>
+    ) : <></>
+  }; */
+
+  const Explore = () => {
+    return (
+      <>
+        <NavBar/>
+        <div className="Map-container">
+          <Map />
+        </div>
         
       </>
-    );
-  };
+    )
+  }
   
   export default Explore;
