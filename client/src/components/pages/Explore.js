d38a1f9fa77a6402a3558fde0afec699681ba7c9
dennis <dennisliu010906@gmail.com>
2024-03-03 13:59:24 -0500

    created NavBar & added on /Explore

--- client/src/components/pages/Explore.js
+++ client/src/components/pages/Explore.js
@@ -2,11 +2,13 @@ import React from "react";
 
 import "../../utilities.css";
 import "./Explore.css"
+import NavBar from "../modules/NavBar.js";
 
-const Explore = ({ userId, handleLogin, handleLogout }) => {
+const Explore = (props) => {
     return (
       <>
-        
+        <NavBar />
+
       </>
     );
   };
