73d85c58415560ecc7507b2ffe38c2149870a652
stephy <stephdai286@gmail.comgit confit --gobal user.email stephdai286@gmail.comgit config --global user.email stephdai286@gmail.com>
2024-03-03 17:07:44 -0500

    added margins

--- client/src/components/pages/Skeleton.js
+++ client/src/components/pages/Skeleton.js
@@ -18,7 +18,7 @@ const Skeleton = ({ userId, handleLogin, handleLogout }) => {
           
               <p className="bathroom"></p>
               <div>
-                <img src="bathroom.jpeg" id="bathroom-image" height="500" width="1000"></img>
+                <img src="bathroom.jpeg" id="bathroom-image" height="700" width="2000"></img>
                 <div className="text-overlay">
                 <form action="/explore" method="GET">
                   <label htmlFor="name">Enter your city, address, neighborhood, zip code</label>
@@ -39,18 +39,16 @@ const Skeleton = ({ userId, handleLogin, handleLogout }) => {
                 <div>
                   <div className="button-container">
                     <div id="review" onclick="writeReview()">Write a review</div>
+                    <h2>About us</h2>
                     <h3>We understand the struggle of finding a clean, accessible, and private bathroom when you are out and about. Our mission is simple: to make your bathroom-hunting experience easy, convenient, and stress-free. Now with Bathroom Buddy, you can locate nearby bathrooms with a few clicks. No matter where you are, B.B. has got your back. We've curated a database of bathrooms based on accessibility, cleanliness, and privacy, ensuring you find the perfect spot wherever you are. Next time nature calls, let Bathroom Buddy guide you. We're here to make your bathroom breaks a breeze!</h3>
 
-                    <div id="learn" onclick="learnMore()">Learn More</div>
 
 
 
                   </div>
                   
 
-                <button>
-            
-                </button>
+                
                 </div>
             </body>
         
